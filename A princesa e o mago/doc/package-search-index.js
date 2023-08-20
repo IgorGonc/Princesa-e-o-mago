3042f45547cc32f2c17e76f935bcacce56329914
Igor Gonçalves <59848422+IgorGonc@users.noreply.github.com>
2023-08-20 20:52:44 -0300

    Limpeza de código

--- A princesa e o mago/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"exercicios3"}];updateSearchResults();
--- A princesa e o mago/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"exercicios3","l":"Lista3"}];updateSearchResults();
